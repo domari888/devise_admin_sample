a5d147cddeb5cc4d18249cbdf02e3cf398fc916d
damari <motokin128@gmail.com>
2021-06-14 21:38:08 +0900

    notifications コントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, path: "admin"
   namespace :admin do
     root "homes#index"
+    resources :notifications
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
