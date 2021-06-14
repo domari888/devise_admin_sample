3d87b2afbdc8b104e2f1831c0bec05d5575bae7a
damari <motokin128@gmail.com>
2021-06-14 17:49:45 +0900

    管理者画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, path: "admin"
+  namespace :admin do
+    root "homes#index"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
